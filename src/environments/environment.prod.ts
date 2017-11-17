73b15b382bbefe2bdadc16866d606d1fd088c7af
Mahuya2 <mahuya.jana@gmail.com>
2017-11-17 04:47:36 -0800

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  // userURL: 'https://user-mgmt_srvc_aares_jenkins.cfapps.io/user',
+  // policyURL: 'https://policy_management_service.cfapps.io/policies'
+  userURL: 'https://user-service-spring-boot-master.cfapps.io/user',
+  policyURL: 'https://mj-policy-management-service.cfapps.io/policies'
+};
